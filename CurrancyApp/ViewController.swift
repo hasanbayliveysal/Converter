c4f0a82fd1e5a6e4180551ebc7d4dba8713900ea
hasanbayliveysal <hasanbayliveysaldev@gmail.com>
2023-04-09 15:52:19 +0400

    modified

--- CurrancyApp/ViewController.swift
+++ CurrancyApp/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    private lazy var label : UILabel = {
+        
+        let label = UILabel()
+        view.addSubview(label)
+        label.text = "Hello"
+        label.font = UIFont.systemFont(ofSize: 59, weight: .medium)
+        return label
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
+        label.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
     }
 
+    
+    
+    
 
 }
 
